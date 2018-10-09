08d2cc5737c5661e0dc6d1f7ceee2ba2e044b75f
Michele Fiorito <fiore.ansaya@gmail.com>
2018-10-09 10:49:56 +0000

    Installation script compelted and external header-only libraries added to the project

--- install.sh
+++ install.sh
@@ -2,38 +2,72 @@
 
 set -e # Abort if any command fails
 
+if [ $1 -eq "clean" ]; then
+	sudo rm -r wiringpi-ansaya
+	sudo rm -r hap-ansaya
+	sudo rm -r LibDomoticPi/release
+	sudo rm -r DomoticPi/release
+	exit 0
+fi
+
 echo 'Checking DomoticPi dependencies'
 
-if ls /usr/local/lib | grep wirningPi.so.2.46-ansaya >/dev/null then
-	echo 'WiringPi by Ansaya is already installed'
+if ldconfig -p | grep libmosquitto >/dev/null; then
+	echo '  - Mosquitto MQTT library ... installed'
 else
-	echo 'Installing WiringPi by Ansaya'
+	echo '  - Mosquitto MQTT library ... installation needed'
+	sudo apt-get install -y libmosquitto-dev
+fi
+
+if ls /usr/local/lib | grep wiringPi.so.2.47-ansaya >/dev/null; then
+	echo '  - WiringPi by Ansaya ... installed'
+else
+	echo '  - WiringPi by Ansaya ... installation needed'
+	if [ -d "wiringpi-ansaya" ]; then
+		sudo rm -r wiringpi-ansaya
+	fi
 	git clone https://github.com/Ansaya/wiringPi.git wiringpi-ansaya
-	./wiringpi-ansaya/build
+	cd wiringpi-ansaya
+	chmod +x build
+	./build
+	cd ..
+	sudo rm -r wiringpi-ansaya
 fi
 
-if ldconfig -p | grep libhap >/dev/null then
-	echo 'HAP library by Ansaya is already installed'
+if ls /usr/local/lib | grep libhap >/dev/null; then
+	echo '  - HAP library by Ansaya ... installed'
 else
-	echo 'Installing HAP library by Ansaya'
+	echo '  - HAP library by Ansaya ... installation needed'
+	if [ -d "hap-ansaya" ]; then
+		sudo rm -r hap-ansaya
+	fi
 	git clone https://github.com/Ansaya/Personal-HomeKit-HAP.git hap-ansaya
-	./hap-ansaya/install.sh
+	cd hap-ansaya
+	chmod +x install.sh
+	./install.sh
+	cd ..
+	sudo rm -r hap-ansaya
 fi
 
-echo 'Dependencies ready\n\nCompiling domoticPi library...'
-cd LibDomoticPi
-mkdir libdomoticPi-release
-cd libdomoticPi-release
+echo 'Dependencies ready'
+echo ''
+echo 'Compiling domoticPi library...'
+mkdir -p LibDomoticPi/release
+cd LibDomoticPi/release
 cmake -DCMAKE_BUILD_TYPE=RELEASE ..
 make -j4
 sudo make install
+sudo ldconfig		#Register new library
 
 echo 'Compiling domoticPi service...'
-cd ../DomoticPi
-mkdir domoticPi-release
-cd domoticPi-release
+cd ../../DomoticPi
+mkdir -p release
+cd release
 cmake -DCMAKE_BUILD_TYPE=RELEASE ..
 make -j4
 sudo make install
 
-ehco '\n\nDomoticPi service succesfully installed!\n\nCreate a new nodeConfig.json under /usr/local/etc/domoticPi directory before starting the service.'
+echo ''
+echo 'DomoticPi service succesfully installed!'
+echo ''
+echo 'Create a new nodeConfig.json under /usr/local/etc/domoticPi directory before starting the service.'
